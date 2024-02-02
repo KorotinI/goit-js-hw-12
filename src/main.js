d26d7c5cd3467c3f16b84283ab167e412f5b0ed7
KorotinI <142391798+KorotinI@users.noreply.github.com>
2024-02-02 10:18:00 +0100

    test comm 1

---
+++ src/main.js
@@ -0,0 +1,176 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '41971380-5e7df6cf95dc1cfc66e370c4e';
+
+const refs = {
+  form: document.getElementById('search-form'),
+  resultContainer: document.getElementById('result-container'),
+  loader: document.querySelector('.loader'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
+
+const hiddenClass = 'is-hidden';
+const params = {
+  page: 1,
+  query: '',
+  maxPage: 0,
+  perPage: 40,
+};
+
+refs.form.addEventListener('submit', handleSearch);
+
+function handleError(message) {
+  iziToast.error({
+    position: 'topRight',
+    message: message,
+  });
+}
+
+async function handleSearch(event) {
+  event.preventDefault();
+  refs.resultContainer.innerHTML = '';
+  params.page = 1;
+
+  refs.loader.classList.remove(hiddenClass);
+  refs.loadMoreBtn.classList.add(hiddenClass);
+  refs.loadMoreBtn.addEventListener('click', handleLoadMore);
+  const form = event.currentTarget;
+  params.query = form.elements.query.value.trim();
+
+  if (!params.query) {
+    refs.loader.classList.add(hiddenClass);
+    return;
+  }
+
+  try {
+    const { hits, totalHits } = await searchPicturesByParams(params);
+
+    if (hits.length === 0) {
+      handleError(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    }
+
+    params.maxPage = Math.ceil(totalHits / params.perPage);
+
+    createMarkup(hits);
+
+    if (hits.length > 0 && hits.length !== totalHits) {
+      refs.loadMoreBtn.classList.remove(hiddenClass);
+    }
+
+    if (params.page === params.maxPage) {
+      handleError("That's all we find!");
+    }
+  } catch (err) {
+    handleError('Something wrong!');
+  } finally {
+    form.reset();
+    refs.loader.classList.add(hiddenClass);
+  }
+}
+
+async function handleLoadMore() {
+  params.page += 1;
+  refs.loader.classList.remove(hiddenClass);
+  refs.loadMoreBtn.disabled = true;
+  try {
+    const { hits } = await searchPicturesByParams(params);
+
+    createMarkup(hits);
+    scrollDown();
+  } catch (err) {
+    handleError('Something wrong!');
+  } finally {
+    refs.loader.classList.add(hiddenClass);
+    refs.loadMoreBtn.disabled = false;
+  }
+  if (params.page === params.maxPage) {
+    refs.loadMoreBtn.classList.add(hiddenClass);
+    refs.loadMoreBtn.removeEventListener('click', handleLoadMore);
+
+    handleError("That's all we find!");
+  }
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function searchPicturesByParams({ query, page = 1, perPage }) {
+  return axios
+    .get(`${BASE_URL}/`, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: perPage,
+        page,
+      },
+    })
+    .then(({ data }) => data);
+}
+
+function createMarkup(hits) {
+  const markUp = hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<li class="gallery-item">
+            <a href="${largeImageURL}">
+  <img class="gallery-image" src="${webformatURL}" alt="${tags}" width="360" heigth="200"></a>
+  <div class="stats-block">
+         <div class="stats">
+             <h2 class="title">Likes</h2>
+             <p class="amount">${likes}</p>
+         </div>
+         <div class="stats">
+             <h2 class="title">Views</h2>
+             <p class="amount">${views}</p>
+         </div>
+          <div class="stats">
+              <h2 class="title">Comments</h2>
+             <p class="amount">${comments}</p>
+         </div>
+          <div class="stats">
+             <h2 class="title">Downloads</h2>
+             <p class="amount">${downloads}</p>
+          </div>
+
+   </div>
+</li>`
+    )
+    .join('');
+  refs.resultContainer.insertAdjacentHTML('beforeend', markUp);
+  lightbox.refresh();
+}
+
+function scrollDown() {
+  if (params.page > 1) {
+    const scrollItem = document
+      .querySelector('.gallery-item')
+      .getBoundingClientRect();
+    window.scrollBy({
+      top: scrollItem.height * 2,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+}
